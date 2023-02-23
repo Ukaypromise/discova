0a37b3f91a9928c4112fd24e03bfd7b71f6574fd
Ukaypromise <promiseukaimo@gmail.com>
2023-02-23 02:15:57 +0100

    Buggyy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/profile'
   devise_for :users, controllers:{
-    sessions: 'users/session',
+    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   get '/u/:id', to: 'users#profile', as: 'user'
