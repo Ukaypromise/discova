4fbd17c1b7eeaa04e29e631478d2f38e75482f9e
Ukaypromise <promiseukaimo@gmail.com>
2023-03-02 23:34:12 +0100

    Users can add image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_one_attached :image
+    has_rich_text :body
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length:{minimum:5, maximum:50}
     validates :body, presence: true, length: {minimum:10, maximum:500} 
