78e9bdfed125c6e8d32f9ebef1974cf9c8c9531b
Ukaypromise <promiseukaimo@gmail.com>
2023-04-12 16:37:18 +0100

    Add notification

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true, length:{minimum:5, maximum:50}
+    validates :body, presence: true, length: {minimum:10, maximum:500} 
+
     belongs_to :user
     has_one_attached :image
     has_rich_text :body
     has_many :comments, dependent: :destroy
-    validates :title, presence: true, length:{minimum:5, maximum:50}
-    validates :body, presence: true, length: {minimum:10, maximum:500} 
+    has_noticed_notification model_name: "Notification"
+    has_many :notifications, through: :user, dependent: :destroy
 end
