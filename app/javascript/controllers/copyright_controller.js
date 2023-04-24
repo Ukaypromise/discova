c0d61aa1d0ccc27d91463653b25a7d3e38524d9a
Ukaypromise <promiseukaimo@gmail.com>
2023-04-24 20:55:09 +0100

    Render current year on footer

--- app/javascript/controllers/copyright_controller.js
+++ app/javascript/controllers/copyright_controller.js
@@ -3,5 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="copyright"
 export default class extends Controller {
   connect() {
+    const currentYear = new Date().getFullYear();
+    this.element.innerHTML = `© ${currentYear}`;
   }
 }
