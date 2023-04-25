a4135ff625e0969884c95c9c01414078c33e47dd
Ukaypromise <promiseukaimo@gmail.com>
2023-04-25 10:16:10 +0100

    Update profile section of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   before_action :set_user
   def profile
     @user.update(views: @user.views + 1)
+    @user = User.find(params[:id])
+    @posts = @user.post.all
   end
+
+  
 
   private
   
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,15 @@ class Comment < ApplicationRecord
   has_rich_text :body
 
   after_create_commit :notify_recipient
+  before_destroy :cleanup_notifications
+  has_noticed_notifications model_name: "Notification"
+
+private
+  def notify_recipient
+    CommentNotification.with(comment: self, post: post).deliver_later(post.user)
+  end
+
+  def cleanup_notifications
+    notifications_as_comments.destroy_all
+  end
 end
