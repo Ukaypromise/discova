9e452ac4a55c1f6dec1c7b559d12155f27335842
Ukaypromise <promiseukaimo@gmail.com>
2023-04-25 11:29:22 +0100

    Fix error with updating views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,9 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    unless current_user == @post.user
     @post.update(views: @post.views + 1)
+    end
     @comments = @post.comments.order(created_at: :desc)
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user
   def profile
-    @user.update(views: @user.views + 1)
-    @user = User.find(params[:id])
+    if @user != current_user
+      @user.update(views: @user.views + 1)
+    end
     @posts = @user.post.all
   end
 
