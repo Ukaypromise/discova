b10c4355828c8890c3ca24bc2576505878f7bacd
Ukaypromise <promiseukaimo@gmail.com>
2023-04-27 11:02:01 +0100

    Notification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :update_allowed_parameters, if: :devise_controller?
+    before_action :set_notifications, if: :current_user
     # before_action :authenticate_user!
 
    
@@ -19,4 +20,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource_or_scope)
     root_path
   end
+
+  private
+  def set_notifications
+    @unread= Notification.where(recipient: current_user).newest_first.limit(9).unread
+  end 
 end
