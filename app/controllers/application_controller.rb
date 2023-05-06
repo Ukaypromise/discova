355446a2b4d96ccea4328a5b6e2f8708139d757c
Ukaypromise <promiseukaimo@gmail.com>
2023-05-06 13:26:30 +0100

    Fix break

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      u.permit(:name, :email, :password, :password_confirmation, :current_password, :image)
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :post, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_one_attached :image
   has_many :notifications, as: :recipient, dependent: :destroy
   
   
