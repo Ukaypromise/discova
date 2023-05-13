929116a31d4a04e78e84223510657bdad119557e
Ukaypromise <promiseukaimo@gmail.com>
2023-05-13 23:54:07 +0100

    Add field and styles

--- app/javascript/controllers/post_sort_controller.js
+++ app/javascript/controllers/post_sort_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="post-sort"
 export default class extends Controller {
+  static targets =["select"]
   connect() {
     console.log('Connecting to data-controller', this.element)
+    console.log(this.selectTarget.value)
   }
 }
