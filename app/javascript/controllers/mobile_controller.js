148f07108f559b3f00cf9883b9737ba75236fd03
Ukaypromise <promiseukaimo@gmail.com>
2023-07-15 19:52:57 +0100

    Add styles to nav

--- app/javascript/controllers/mobile_controller.js
+++ app/javascript/controllers/mobile_controller.js
@@ -3,9 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="mobile"
 export default class extends Controller {
   static targets = ["toggle"];
-  connect() {
-    console.log("mobile controller connected");
-  }
   toggleDropdown() {
     console.log("Helloooooo");
     this.toggleTarget.classList.toggle("hidden");
