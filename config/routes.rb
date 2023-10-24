17487c79f62719ae3db935d40e4401ff68309fc9
Ukaypromise <promiseukaimo@gmail.com>
2023-10-24 21:35:13 +0100

    Learnt about turbo fram tags and turbo streams with their views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :quotes
   get 'friends/index'
   get 'users/profile'
   devise_for :users, controllers:{
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_213122) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_142818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_07_213122) do
     t.integer "views", default: 0
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "social_handles", force: :cascade do |t|
